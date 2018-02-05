f21d0a132c8137aec5791e0f3f37a3055a1812a8
Aequinoxio <prikeprok@gmail.com>
2018-02-05 06:31:31 +0100

    Bugfix:  - DB's reinitialization was not properly working if is called the second time after the program started. Added:  - DB reinitializazion feature (some bug was removed)  - Right click popup menu with single thumbnail reload function

--- src/it/aequinoxio/speeddialutility/SpeedDialMainFrame.java
+++ src/it/aequinoxio/speeddialutility/SpeedDialMainFrame.java
@@ -7,9 +7,7 @@ package it.aequinoxio.speeddialutility;
 
 import DUMMY_TODEL.MainFrame;
 import java.awt.Desktop;
-import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.event.ItemEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -22,10 +20,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.prefs.Preferences;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
@@ -33,11 +29,18 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
-import Utilities.Constants;
 import Utilities.CustomPreferences;
-import it.aequinoxio.speeddialutility.SQLliteUtils;
-import it.aequinoxio.speeddialutility.ManageConfiguration;
 import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.SwingWorker;
 
 /**
  *
@@ -48,6 +51,10 @@ public class SpeedDialMainFrame extends javax.swing.JFrame {
     SQLliteUtils sqlu;
     ArrayList<String> siteUrls;
     CustomPreferences prefs = CustomPreferences.getInstance();
+    MouseListener ml;
+    TreeSelectionListener tsl;
+    DefaultMutableTreeNode selectedNode; // Nodo selezionato con il tasto destro
+    static JFrame MYSELF;
 
 //GridLayout groupImages = new GridLayout(0,3); // tre colonne e infinite righe
     /**
@@ -58,59 +65,77 @@ public class SpeedDialMainFrame extends javax.swing.JFrame {
      */
     public SpeedDialMainFrame() throws SQLException, ClassNotFoundException {
         initComponents();
+        setLocationRelativeTo(null);
 
-        initTree();
-    }
+        MYSELF = this;
 
-    private void initTree()throws SQLException, ClassNotFoundException {
-        //CustomPreferences.getInstance().resetPreferencesToDefault(); // Inizializzo le preferences. TODO: Trovare un modo migliore per lanciarlo indipendentemente dalla classe frame che mostrerà la finestra
-
-        this.sqlu = new SQLliteUtils(prefs.getDBPath());
-        DefaultMutableTreeNode top = new DefaultMutableTreeNode("SpeedDial urls");
-        
-        createNodes(top);
-        
-        treeSites.setModel(new DefaultTreeModel(top, false));
-        treeSites.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-
-        MouseListener ml = new MouseAdapter() {
+        ml = new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 int selRow = treeSites.getRowForLocation(e.getX(), e.getY());
                 TreePath selPath = treeSites.getPathForLocation(e.getX(), e.getY());
+                treeSites.setSelectionPath(selPath);
+
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    if (selPath != null) {
+                        selectedNode = (DefaultMutableTreeNode) selPath.getLastPathComponent();
+                    } else {
+                        selectedNode = null;
+                    }
+                }
 
                 if (selRow != -1) {
-                    if (e.getClickCount() == 1) {
+                    switch (e.getClickCount()) {
                         // Non faccio nulla
-                    } else if (e.getClickCount() == 2) { // Al doppio click apro l'url nel browser
-                        Object o = selPath.getLastPathComponent();
-                        Object uo;
-                        if (o instanceof DefaultMutableTreeNode) {
-                            uo = ((DefaultMutableTreeNode) o).getUserObject();
-                            if (uo instanceof SiteInfo) {
-                                int retVal = JOptionPane.showConfirmDialog(null, String.format("Apro il sito%n%s", ((SiteInfo) uo).url));
-                                if (retVal == JOptionPane.OK_OPTION) {
-                                    try {
-                                        Desktop.getDesktop().browse(new URI(((SiteInfo) uo).url));
-                                    } catch (URISyntaxException | IOException ex) {
-                                        Logger.getLogger(SpeedDialMainFrame.class.getName()).log(Level.SEVERE, null, ex);
+                        case 1:
+                            break;
+                        case 2:
+                            // Al doppio click apro l'url nel browser
+                            Object o = selPath.getLastPathComponent();
+                            Object uo;
+                            if (o instanceof DefaultMutableTreeNode) {
+                                uo = ((DefaultMutableTreeNode) o).getUserObject();
+                                if (uo instanceof SiteInfo) {
+                                    int retVal = JOptionPane.showConfirmDialog(null, String.format("Apro il sito%n%s", ((SiteInfo) uo).url));
+                                    if (retVal == JOptionPane.OK_OPTION) {
+                                        try {
+                                            Desktop.getDesktop().browse(new URI(((SiteInfo) uo).url));
+                                        } catch (URISyntaxException | IOException ex) {
+                                            Logger.getLogger(SpeedDialMainFrame.class.getName()).log(Level.SEVERE, null, ex);
+                                        }
                                     }
                                 }
                             }
-                        }
-                        
-                    } else{ // click count != 2
-                        
+                            break;
+                        // click count != 2
+                        default:
+                            break;
                     }
+                    treeSites.setSelectionRow(selRow);
                 }
             }
-        };
-        treeSites.addMouseListener(ml);
 
-        treeSites.addTreeSelectionListener(new TreeSelectionListener() {
+//            @Override
+//            public void mouseReleased(MouseEvent e) {
+//                super.mouseReleased(e); //To change body of generated methods, choose Tools | Templates.
+//                int selRow = treeSites.getRowForLocation(e.getX(), e.getY());
+//                TreePath selPath = treeSites.getPathForLocation(e.getX(), e.getY());
+//                treeSites.setSelectionPath(selPath);
+//
+//                if (e.isPopupTrigger()) {
+//                    if (selPath != null) {
+//                        selectedNode = (DefaultMutableTreeNode) selPath.getLastPathComponent();
+//                    } else {
+//                        selectedNode = null;
+//                    }
+//                }
+//
+//            }
+        };
+
+        tsl = new TreeSelectionListener() {
             @Override
-            public void valueChanged(TreeSelectionEvent tse
-            ) {
+            public void valueChanged(TreeSelectionEvent tse) {
                 DefaultMutableTreeNode node = (DefaultMutableTreeNode) treeSites.getLastSelectedPathComponent();
                 lblImage.setVisible(true);
                 lblUrl.setText(" ");
@@ -141,8 +166,58 @@ public class SpeedDialMainFrame extends javax.swing.JFrame {
                     lblImage.setVisible(false);
                 }
             }
+        };
+
+        JPopupMenu menu = new JPopupMenu();
+        JMenuItem item = new JMenuItem("Ricarica thumbnail");
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                Object uo = selectedNode.getUserObject();
+                if (uo instanceof SiteInfo) {
+                    int retVal = JOptionPane.showConfirmDialog(null, String.format("Ricarico il thumbnail del sito%n%s", ((SiteInfo) uo).url));
+                    if (retVal == JOptionPane.OK_OPTION) {
+                        try {
+                            sqlu.closeDBConnection();
+                            sqlu = null;
+
+                            UpdateSingleThumbnail updateSingleThumbnail = new UpdateSingleThumbnail(MYSELF, true);
+                            updateSingleThumbnail.setLocationRelativeTo(MYSELF);
+                            updateSingleThumbnail.grabThumbnail(((SiteInfo) uo).url);
+                            updateSingleThumbnail.setVisible(true);
+                            initTree();
+                        } catch (SQLException ex) {
+                            Logger.getLogger(SpeedDialMainFrame.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (ClassNotFoundException ex) {
+                            Logger.getLogger(SpeedDialMainFrame.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+            }
+        });
+        menu.add(item);
+        treeSites.setComponentPopupMenu(menu);
+
+        initTree();
+    }
+
+    private void initTree() throws SQLException, ClassNotFoundException {
+        //CustomPreferences.getInstance().resetPreferencesToDefault(); // Inizializzo le preferences. TODO: Trovare un modo migliore per lanciarlo indipendentemente dalla classe frame che mostrerà la finestra
+
+        if (this.sqlu != null) {
+            sqlu.closeDBConnection();
         }
-        );
+
+        this.sqlu = new SQLliteUtils(prefs.getDBPath());
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode("SpeedDial urls");
+
+        createNodes(top);
+
+        treeSites.setModel(new DefaultTreeModel(top, false));
+        treeSites.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+
+        treeSites.addMouseListener(ml);
+        treeSites.addTreeSelectionListener(tsl);
     }
 
     private void createNodes(DefaultMutableTreeNode top) throws SQLException {
@@ -194,11 +269,14 @@ public class SpeedDialMainFrame extends javax.swing.JFrame {
         jMenu1 = new javax.swing.JMenu();
         mnuRebuildDB = new javax.swing.JMenuItem();
         mnuConfiguration = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        mnuInitDB = new javax.swing.JMenuItem();
 
         jMenuItem1.setText("jMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        treeSites.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         treeSites.setMinimumSize(new java.awt.Dimension(190, 330));
         jScrollPane2.setViewportView(treeSites);
 
@@ -208,7 +286,12 @@ public class SpeedDialMainFrame extends javax.swing.JFrame {
         lblImage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblImage.setText("    ");
         lblImage.setMaximumSize(new java.awt.Dimension(0, 0));
-        lblImage.setMinimumSize(new java.awt.Dimension(80, 60));
+        lblImage.setMinimumSize(new java.awt.Dimension(800, 600));
+        lblImage.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblImageMouseClicked(evt);
+            }
+        });
         jScrollPane3.setViewportView(lblImage);
 
         jSplitPane1.setRightComponent(jScrollPane3);
@@ -232,6 +315,19 @@ public class SpeedDialMainFrame extends javax.swing.JFrame {
             }
         });
         jMenu1.add(mnuConfiguration);
+        jMenu1.add(jSeparator1);
+
+        mnuInitDB.setBackground(new java.awt.Color(255, 0, 0));
+        mnuInitDB.setFont(new java.awt.Font("Segoe UI", 1, 15)); // NOI18N
+        mnuInitDB.setForeground(new java.awt.Color(255, 0, 0));
+        mnuInitDB.setText("Inizializza il database");
+        mnuInitDB.setToolTipText("Cancella il contenuto del database e lo reinizializza ricaricando le url e rigenerando i thumbnails");
+        mnuInitDB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuInitDBActionPerformed(evt);
+            }
+        });
+        jMenu1.add(mnuInitDB);
 
         jMenuBar1.add(jMenu1);
 
@@ -264,6 +360,7 @@ public class SpeedDialMainFrame extends javax.swing.JFrame {
     private void mnuConfigurationActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuConfigurationActionPerformed
         ManageConfiguration showConfiguration = new ManageConfiguration();
         showConfiguration.setModalExclusionType(Dialog.ModalExclusionType.NO_EXCLUDE);
+        showConfiguration.setLocationRelativeTo(this);
         showConfiguration.setVisible(true);
     }//GEN-LAST:event_mnuConfigurationActionPerformed
 
@@ -277,6 +374,73 @@ public class SpeedDialMainFrame extends javax.swing.JFrame {
         //CustomPreferences.getInstance().resetPreferencesToDefault();
         // TODO: remove all DB entries and reaload the pref.js (asking the file's location
     }//GEN-LAST:event_mnuRebuildDBActionPerformed
+
+    UpdateDB updateDB = null;
+    private void mnuInitDBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuInitDBActionPerformed
+        if (JOptionPane.showConfirmDialog(this, "Ripristino tutto?\nQuesto azzererà il DB corrente ed avvierà il parsing del file prefs.js\ned il downlo adelle immagini. Il processo può essere molto lungo.")
+                == JOptionPane.OK_OPTION) {
+
+            String latestPath = CustomPreferences.getInstance().getLatestFilePath();
+            File fTemp = null;
+            if (latestPath != null) {
+                fTemp = new File(latestPath);
+            }
+            final JFileChooser fc = new JFileChooser(fTemp);
+
+            if (fc.showDialog(this, "Apri il file prefs.js") == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //todo metterla nelle prefs e ricaricare il db
+                CustomPreferences.getInstance().setLatestFilePath(file.getAbsolutePath());
+                if (updateDB == null) {
+                    updateDB = new UpdateDB(this, true, file.getAbsolutePath());
+                    updateDB.addWindowListener(new WindowAdapter() {
+                        @Override
+                        public void windowClosed(WindowEvent we) {
+                            super.windowClosed(we); //To change body of generated methods, choose Tools | Templates.
+                            try {
+                                initTree();
+                                updateDB = null; // Sembra necessario in quanto i thread non si chiudono anche se cancellati esplicitamente. Se non rigenero tutto viene creato un altro thread al comando sdu.execute()
+                            } catch (SQLException | ClassNotFoundException ex) {
+                                Logger.getLogger(SpeedDialMainFrame.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                    });
+                }
+
+                try {
+                    updateDB.setLocationRelativeTo(this);
+                    // Per l'update di un singolo thumbnail occorre choidere esplicitamente il DB. Viene riaperto in initTree()
+                    sqlu.closeDBConnection();
+                    sqlu = null;
+
+                    updateDB.startWork(); // Se updateDB è modale occorre far partire tutti i metodi prima di mostrare la finestra
+
+                    updateDB.setVisible(true);
+
+                    treeSites.removeMouseListener(ml);
+                    treeSites.removeTreeSelectionListener(tsl);
+                    treeSites.setModel(null);
+                } catch (SQLException ex) {
+                    Logger.getLogger(SpeedDialMainFrame.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+//                SpeedDialUtility sdu = new SpeedDialUtility(file.getAbsolutePath(),logViewer.getLogTextArea());
+//                
+//                //sdu.startImport();
+//                sdu.execute();
+//                JOptionPane.showMessageDialog(this, "Finito");
+                // TODO: Ricaricare il tree
+            } else {
+                JOptionPane.showMessageDialog(this, "Annullato, nessuna modifica apportata al DB");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Annullato, nessuna modifica apportata al DB");
+        }
+    }//GEN-LAST:event_mnuInitDBActionPerformed
+
+    private void lblImageMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblImageMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_lblImageMouseClicked
 
     /**
      * @param args the command line arguments
@@ -330,10 +494,12 @@ public class SpeedDialMainFrame extends javax.swing.JFrame {
     private javax.swing.JPopupMenu jPopupMenu2;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
     private javax.swing.JSplitPane jSplitPane1;
     private javax.swing.JLabel lblImage;
     private javax.swing.JLabel lblUrl;
     private javax.swing.JMenuItem mnuConfiguration;
+    private javax.swing.JMenuItem mnuInitDB;
     private javax.swing.JMenuItem mnuRebuildDB;
     private javax.swing.JTree treeSites;
     // End of variables declaration//GEN-END:variables
